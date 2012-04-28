1ee622bb97ef96ac1bb4f4754826cc7c6674df87
真嶌 晋 <smajima@makototou-no-Mac-mini.local>
2012-04-28 18:45:49 +0900

    initial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MusicSns::Application.config.secret_token = 'c0998e90b2885d0b1320940512d479b017e0ea72c44ff9ce8af67ad092407bde0e042cbe8b86a263248bc0ca2deedea00b5dde48504e98e9ac645f750eff660d'
